86f140ed20fc74bd559f9ce78e532ef6daddf580
Diego Diarte <die.duarte@duocuc.cl>
2025-04-17 10:13:34 -0400

    tercero Commit

--- src/main/java/com/example/Biblioteca/Services/LibroService.java
+++ src/main/java/com/example/Biblioteca/Services/LibroService.java
@@ -3,10 +3,28 @@ import com.example.Biblioteca.Model.Libro;
 import com.example.Biblioteca.Repository.LibroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 public class LibroService {
     @Autowired
     private LibroRepository libroRepository;
+
+    public List<Libro> getLibros(){
+        return libroRepository.obteneLibros();
+    }
+
+    public Libro saveLibro(Libro libro){
+        return libroRepository.guardar(libro);
+    }
+
+    public Libro getLibroId(int id){
+        return libroRepository.buscarPorId(id);
+    }
+
+    public updateLibro(Libro libro){
+        return libroRepository.actualizar(libro);
+        
+    }
 
 }
